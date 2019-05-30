90e4990d9ab6e7ff61f5d3026bd168b35f9497a2
Matias Draeger <matias.draeger@hotmail.com>
2019-05-29 21:54:08 -0300

    App lista

--- app/src/main/java/com/example/jugandoconintents/WebDeAutor.java
+++ app/src/main/java/com/example/jugandoconintents/WebDeAutor.java
@@ -6,12 +6,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class WebDeAutor extends AppCompatActivity {
     private Button btnGithub;
     private Button btnTelefono;
+    private Button buttonConsulta;
     String repositorio="https://www.github.com/matiasdr";
     String telefono="+542954390619";
+    EditText txtConsulta;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +24,14 @@ public class WebDeAutor extends AppCompatActivity {
 
         btnGithub=findViewById(R.id.btnGithub);
         btnTelefono=findViewById(R.id.btnTelefono);
-
+        buttonConsulta=findViewById(R.id.buttonConsulta);
+        txtConsulta=findViewById(R.id.editTextConsulta);
+        buttonConsulta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ConsultaMail();
+            }
+        });
         btnGithub.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -34,6 +45,18 @@ public class WebDeAutor extends AppCompatActivity {
             MarcarTelefono();
             }
         });
+    }
+
+    private void ConsultaMail() {
+        if(txtConsulta.getText().toString().isEmpty()){
+            Toast.makeText(this, "Escribi algo por favor", Toast.LENGTH_SHORT).show();
+        }else {
+            Intent i = new Intent(Intent.ACTION_SENDTO, Uri.parse("mailto:"));
+            i.putExtra(i.EXTRA_EMAIL, "matias.draeger@hotmail.com");
+            i.putExtra(i.EXTRA_SUBJECT, "CONSULTA");
+            i.putExtra(i.EXTRA_TEXT, txtConsulta.getText().toString());
+            startActivity(i);
+        }
     }
 
     void Repositorio(){
